1cefb84d3dad10f4b5b43e8c2db4d537b5bd5a21
shogo.takasaki <stakasaki707@gmail.com>
2024-10-31 00:09:17 +0900

    make your layout adapt to different screen sizes

--- app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -15,6 +15,8 @@
  */
 package com.example.reply.ui
 
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -25,10 +27,26 @@ import com.example.reply.data.MailboxType
 
 @Composable
 fun ReplyApp(
+    windowSize: WindowWidthSizeClass,
     modifier: Modifier = Modifier,
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState by viewModel.uiState.collectAsState()
+
+    when (windowSize) {
+        WindowWidthSizeClass.Compact -> {
+
+        }
+        WindowWidthSizeClass.Medium -> {
+
+        }
+        WindowWidthSizeClass.Expanded -> {
+
+        }
+        else -> {
+
+        }
+    }
 
     ReplyHomeScreen(
         replyUiState = replyUiState,
